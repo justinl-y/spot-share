cd55381df63ebc24f0fdb30365c1eb0336dcee33
Djordje Vujatovic <djordje_v@hotmail.com>
2017-02-15 19:08:38 -0800

    Added styles to errorStyle and labelStyle in signUp form.

--- imports/ui/components/SignUp/signUp.js
+++ imports/ui/components/SignUp/signUp.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Meteor } from 'meteor/meteor';
+import { cyan500 } from 'material-ui/styles/colors';
+
+
+const styles = {
+  floatingLabelStyle: {
+    color: cyan500,
+  },
+  errorStyle: {
+    color: cyan500,
+  },
+};
 
 class SignUp extends Component {
   constructor() {
     super();
     this.submitAction = this.submitAction.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
   }
   submitAction(event) {
-    event.preventDefault();
-    const signUpForm = $(event.target);
-
-    const email = signUpForm.find('#email').val();
-    const password = signUpForm.find('#password').val();
-    const confirmPassword = signUpForm.find('#confirm-password').val();
+    const { email, password, confirmPassword } = this.state;
 
     if (password === confirmPassword && password !== '' && confirmPassword !== '') {
       const accountInfo = {
@@ -35,30 +48,62 @@ class SignUp extends Component {
     } else {
       console.log('Your passwords do not match!');
     }
-
+  }
+  handleInputChange(input, event) {
+    const updateState = {};
+    updateState[input] = event.target.value;
+    this.setState(updateState);
   }
   // createUser
 
 
   render() {
     return (
-      <form onSubmit={this.submitAction}>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input placeholder="Email" type="email" id="email" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input placeholder="Password" type="password" id="password" className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Confirm Password:</label>
-          <input placeholder="Confirm Password" type="password" id="confirm-password" className="form-control" />
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-primary">Button</button>
-        </div>
-      </form>
+             <div>
+                <TextField
+                  style={{
+                    width: '100%',
+                  }}
+                  hintText="Email"
+                  errorText="Please enter your email."
+                  floatingLabelText="Email"
+                  errorStyle={styles.errorStyle}
+                  floatingLabelStyle={styles.floatingLabelStyle}
+                  onChange={(event) => { this.handleInputChange('email', event)}}
+
+                /><br />
+
+                <TextField
+                  style={{
+                    width: '100%',
+                  }}
+                  hintText="Password"
+                  errorText="Please enter your password."
+                  floatingLabelText="Password"
+                  theme="primary"
+                  errorStyle={styles.errorStyle}
+                  floatingLabelStyle={styles.floatingLabelStyle}
+                  onChange={(event) => { this.handleInputChange('password', event)}}
+                /><br />
+
+                <TextField
+                  style={{
+                    width: '100%',
+                  }}
+                  hintText="Confirm Password"
+                  errorText="Please enter your password."
+                  floatingLabelText="Confirm Password"
+                  errorStyle={styles.errorStyle}
+                  floatingLabelStyle={styles.floatingLabelStyle}
+                  onChange={(event) => { this.handleInputChange('confirmPassword', event)}}
+                /><br />
+
+                <RaisedButton
+                  labelColor="black"
+                  label="Sign Up"
+                  onClick={(e) => { this.submitAction(e); }}
+                />
+              </div>
     );
   }
 }
