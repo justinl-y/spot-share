5dbcdbdb49bbe24abc0de515afddbacf5b89768f
Jonathan Funk <jonfunk88@hotmail.com>
2017-02-17 09:50:52 -0800

    link added to InfoBox

--- imports/ui/components/InfoBox/index.js
+++ imports/ui/components/InfoBox/index.js
@@ -1,12 +1,15 @@
 import React, { PropTypes } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router';
 
 const InfoBox = ({ address, price, info }) => (
   <div style={{ width: '200px', height: '200px' }}>
     <h2>{address}</h2>
     <p>Price per hour: {price}</p>
     <p>{info}</p>
-    <RaisedButton label="Book Space" />
+    <Link to="/bookspot/list">
+      <RaisedButton label="Book Space" />
+    </Link>
   </div>
 );
 
