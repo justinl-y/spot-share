242a96d24c2def69d6a824b75ea6a348a21067b5
Djordje Vujatovic <djordje_v@hotmail.com>
2017-02-19 12:26:02 -0800

    Added props validation and edit and delete props to Booking.

--- imports/ui/components/Booking/index.js
+++ imports/ui/components/Booking/index.js
@@ -7,23 +7,26 @@ const styles = {
   },
 };
 
-const Booking = ({ id, userId, bookingCost, duration, parkingSpotId, dateBooked, timeBooked }) => (
+const Booking = ({ id, userId, bookingCost, duration, parkingSpotId, dateBooked, timeBooked, editBookingSpot, deleteBookingSpot }) => (
   <li style={styles.component}>
     <h3>Booking ID: { id }</h3>
     <p>User ID: {userId} | Duration: { duration } | Booking Cost: { bookingCost } | Parking Spot ID: { parkingSpotId }</p>
     <p>Date Booked: { dateBooked } | Time Booked: { timeBooked }</p>
-    <button onClick={(e) => { e.preventDefault(); console.log('Edit Clicked'); }}>Edit</button>
-    <button onClick={(e) => { e.preventDefault(); console.log('Eliminate Charlie'); }}>Delete</button>
+    <button onClick={(e) => { e.preventDefault(); editBookingSpot(id); }}>Edit</button>
+    <button onClick={(e) => { e.preventDefault(); deleteBookingSpot(id); }}>Delete</button>
   </li>
 );
 
 Booking.propTypes = {
   id: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired,
   bookingCost: PropTypes.number.isRequired,
   duration: PropTypes.number.isRequired,
   parkingSpotId: PropTypes.string.isRequired,
   dateBooked: PropTypes.string.isRequired,
   timeBooked: PropTypes.string.isRequired,
+  editBookingSpot: PropTypes.func.isRequired,
+  deleteBookingSpot: PropTypes.func.isRequired,
 };
 
 export default Booking;
