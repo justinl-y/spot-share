8016bc8737543ec5ea93cb7ae7b56535b3a1febe
Djordje Vujatovic <djordje_v@hotmail.com>
2017-02-19 11:45:03 -0800

    Added PropTypes to Booking component.

--- imports/ui/components/Booking/index.js
+++ imports/ui/components/Booking/index.js
@@ -12,14 +12,19 @@ const Booking = ({ id, bookingCost, duration, parkingSpotId, dateBooked, timeBoo
     <h3>Booking ID: { id }</h3>
     <p>Duration: { duration } | Booking Cost: { bookingCost } | Parking Spot ID: { parkingSpotId }</p>
     <p>Date Booked: { dateBooked } |Time Booked: { timeBooked }</p>
-    <button onClick={ (e)=> {e.preventDefault(); console.log('Edit Clicked')}}>Edit</button>
-    <button onClick={ (e)=> {e.preventDefault(); console.log('Eliminate Charlie')}}>Delete</button>
+    <button onClick={(e) => { e.preventDefault(); console.log('Edit Clicked')}}>Edit</button>
+    <button onClick={(e) => { e.preventDefault(); console.log('Eliminate Charlie')}}>Delete</button>
 
   </li>
 );
 
 Booking.propTypes = {
   id: PropTypes.string.isRequired,
+  bookingCost: PropTypes.number.isRequired,
+  duration: PropTypes.number.isRequired,
+  parkingSpotId: PropTypes.string.isRequired,
+  dateBooked: PropTypes.string.isRequired,
+  timeBooked: PropTypes.string.isRequired,
 };
 
 
