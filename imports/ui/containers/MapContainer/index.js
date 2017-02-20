4933159b20af1668f096126b60ecb24e19a02fa9
Jonathan Funk <jonfunk88@hotmail.com>
2017-02-19 16:34:20 -0800

    added customized markers to share spot map

--- imports/ui/containers/MapContainer/index.js
+++ imports/ui/containers/MapContainer/index.js
@@ -5,6 +5,7 @@ import InfoBox from './../../components/InfoBox'
 import { withGoogleMap, GoogleMap, InfoWindow, Marker } from 'react-google-maps';
 import SearchBox from './lib/places/SearchBox';
 import RaisedButton from 'material-ui/RaisedButton';
+import blueIcon from './blueIcon';
 
 //Styles for the mapContainer & intoBox
 const styles = {
@@ -32,16 +33,6 @@ const INPUT_STYLE = {
   outline: `none`,
   textOverflow: `ellipses`,
 };
-
-//This creates a custom SVG icon 14px high 20px wide 
-const blueIcon = {
-          path: 'M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z',
-          fillColor: '#00BCD4',
-          fillOpacity: 1,
-          strokeWeight: 0,
-          anchor: new google.maps.Point(7,20),
-          scale: 2
-        };
 
 
 //This renders Google Map
